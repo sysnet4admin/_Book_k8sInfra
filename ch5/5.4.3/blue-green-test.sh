4fb719f13b7b32c6b10b67ed854b1675e90e4fe6
sysnet4admin <pagaia@hotmail.com>
2020-11-01 11:12:16 +0900

    upt test-code name

--- ch5/5.4.3/blue-green-test.sh
+++ ch5/5.4.3/blue-green-test.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-deploy_func="kubectl get deployments -o jsonpath --template={.items[0].status.readyReplicas}"
+kgd_out="kubectl get deployments -o jsonpath --template={.items[0].status.readyReplicas}"
 while true; do
-  green_ready=$($deploy_func --selector=app=dashboard,deploy=green 2> /dev/null)
-  blue_ready=$($deploy_func --selector=app=dashboard,deploy=blue 2> /dev/null)
+  green_ready=$($kgd_out --selector=app=dashboard,deploy=green 2> /dev/null)
+  blue_ready=$($dkgd_out --selector=app=dashboard,deploy=blue 2> /dev/null)
   echo "green: $green_ready / blue: $blue_ready"
   sleep 1;
 done
