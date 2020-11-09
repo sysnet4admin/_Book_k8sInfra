39f794496a570b78e3c73016b72619790730e1e3
sysnet4admin <pagaia@hotmail.com>
2020-11-10 07:12:43 +0900

    upt ch5 all

---
+++ ch5/5.3.1/jenkins-install.sh
@@ -0,0 +1,13 @@
+#/bin/env bash
+helm install jenkins \
+--set persistence.existingClaim=jenkins \
+--set master.adminPassword=admin \
+--set master.nodeSelector."kubernetes\.io/hostname"=m-k8s \
+--set master.tolerations[0].key=node-role.kubernetes.io/master \
+--set master.tolerations[0].effect=NoSchedule \
+--set master.tolerations[0].operator=Exists \
+--set master.runAsUser=1000 \
+--set master.fsGroup=1000 \
+--set master.tag=2.249.3-lts-centos7 \
+--set master.serviceType=LoadBalancer \
+edu/jenkins
--- ch5/5.4.3/blue-green-test.sh
+++ ch5/5.4.3/blue-green-test.sh
@@ -3,6 +3,6 @@ kgd_out="kubectl get deployments -o jsonpath --template={.items[0].status.readyR
 while true; do
   green_ready=$($kgd_out --selector=app=dashboard,deploy=green 2> /dev/null)
   blue_ready=$($dkgd_out --selector=app=dashboard,deploy=blue 2> /dev/null)
-  echo "green: $green_ready / blue: $blue_ready"
+  echo "blue: $blue_ready / green: $green_ready"
   sleep 1;
 done
