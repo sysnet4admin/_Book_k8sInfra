52b45652edd8ff2fb129a419169da6a52925ba42
sysnet4admin <pagaia@hotmail.com>
2020-11-17 20:34:28 +0900

    upt 5.2

---
+++ ch5/5.2.2/kustomize-install.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+curl -L \
+https://github.com/kubernetes-sigs/kustomize/releases/download/kustomize%2Fv3.6.1/kustomize_v3.6.1_linux_amd64.tar.gz -o /tmp/kustomize.tar.gz
+tar -xzf /tmp/kustomize.tar.gz -C  /usr/local/bin
+echo "kustomize install successfully"
--- ch5/5.4.3/blue-green-test.sh
+++ ch5/5.4.3/blue-green-test.sh
@@ -1,8 +1,8 @@
-#!/bin/bash
+#!/usr/bin/env bash
 kgd_out="kubectl get deployments -o jsonpath --template={.items[0].status.readyReplicas}"
 while true; do
-  green_ready=$($kgd_out --selector=app=dashboard,deploy=green 2> /dev/null)
-  blue_ready=$($dkgd_out --selector=app=dashboard,deploy=blue 2> /dev/null)
+  blue_ready=$($kgd_out --selector=deploy=blue 2> /dev/null)
+  green_ready=$($kgd_out --selector=deploy=green 2> /dev/null)
   echo "blue: $blue_ready / green: $green_ready"
-  sleep 1;
+  sleep 1
 done
