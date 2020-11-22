1c6ac29db93881b779c4be9e0174770f22532eae
sysnet4admin <pagaia@hotmail.com>
2020-11-22 12:32:05 +0900

    rmv useless items

--- ch5/5.2.2/kustomize-install.sh
+++ ch5/5.2.2/kustomize-install.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+
 curl -L \
 https://github.com/kubernetes-sigs/kustomize/releases/download/kustomize%2Fv3.6.1/kustomize_v3.6.1_linux_amd64.tar.gz -o /tmp/kustomize.tar.gz
 tar -xzf /tmp/kustomize.tar.gz -C  /usr/local/bin
