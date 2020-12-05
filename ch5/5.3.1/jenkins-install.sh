92bc9ea0ad3b5b21e746cac70563a5e45f952d8f
sysnet4admin <pagaia@hotmail.com>
2020-12-05 18:04:32 +0900

    upt jenkins files

--- ch5/5.3.1/jenkins-install.sh
+++ ch5/5.3.1/jenkins-install.sh
@@ -1,7 +1,7 @@
 #/bin/env bash
 jopt1='-DsessionTimeout=1440'
 jopt2='-Duser.timezone=Asia/Seoul'
-jopt3='-Dcasc.jenkins.config=$(dirname "$0")/pod-template.yaml'
+jopt3='-Dcasc.jenkins.config=$(dirname "$0")/jenkins-config.yaml'
 
 helm install jenkins edu/jenkins \
 --set persistence.existingClaim=jenkins \
