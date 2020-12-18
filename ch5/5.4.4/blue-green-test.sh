3c375f89c41680b22df2f723192906645960bf84
sysnet4admin <pagaia@hotmail.com>
2020-12-18 22:43:41 +0900

    upt jenkinsfile

--- ch5/5.4.4/blue-green-test.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-kgd_out="kubectl get deployments -o jsonpath --template={.items[0].status.readyReplicas}"
-while true; do
-  blue_ready=$($kgd_out --selector=deploy=blue 2> /dev/null)
-  green_ready=$($kgd_out --selector=deploy=green 2> /dev/null)
-  echo "blue: $blue_ready / green: $green_ready"
-  sleep 1
-done
