b57765435f7fb1708b8f06a7f6a208c388027a5c
sysnet4admin <pagaia@hotmail.com>
2020-12-25 11:28:45 +0900

    upt ext

--- ch6/6.4.1/grafana-install.sh
+++ ch6/6.4.1/grafana-install.sh
@@ -2,4 +2,7 @@
 helm install grafana edu/grafana \
 --set persistence.enabled=true \
 --set persistence.existingClaim=grafana \
---set service.type=LoadBalancer 
+--set service.type=LoadBalancer \
+--set adminPassword="admin" \ 
+--set securityContext.runAsUser=1012 \
+--set securityContext.runAsGroup=1012
