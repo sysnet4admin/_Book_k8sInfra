fa630106ded9e536e574dc6ab0381a610ecfe476
sysnet4admin <pagaia@hotmail.com>
2021-01-07 18:48:11 +0900

    upt prometheus-install.sh

--- ch6/6.2.1/prometheus-install.sh
+++ ch6/6.2.1/prometheus-install.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 helm install prometheus edu/prometheus \
 --set pushgateway.enabled=false \
+--set alertmanager.enabled=false \
 --set nodeExporter.tolerations[0].key=node-role.kubernetes.io/master \
 --set nodeExporter.tolerations[0].effect=NoSchedule \
 --set nodeExporter.tolerations[0].operator=Exists \
@@ -8,5 +9,4 @@ helm install prometheus edu/prometheus \
 --set server.securityContext.runAsGroup=1000 \
 --set server.securityContext.runAsUser=1000 \
 --set server.service.type="LoadBalancer" \
---set server.extraFlags[0]="storage.tsdb.no-lockfile" \
---set alertmanager.enabled=false
+--set server.extraFlags[0]="storage.tsdb.no-lockfile" 
