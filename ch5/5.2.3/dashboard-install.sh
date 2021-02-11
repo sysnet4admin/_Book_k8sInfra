38e9ac7dd19336b5f148d72bc9be47e12bfd9f3e
sysnet4admin <pagaia@hotmail.com>
2021-02-11 17:57:04 +0900

    rmv dashboard

--- ch5/5.2.3/dashboard-install.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-helm install dashboard edu/dashboard \
---set service.type="LoadBalancer" \
---set dashboard.printName="${USERNAME}"
--- ch5/5.2.3/dashboard-upgrade.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-helm upgrade dashboard edu/dashboard \
---set service.type="LoadBalancer" \
---set dashboard.printName="${USERNAME}" \
---set dashboard.color="green"
