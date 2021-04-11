3bb76e0587bcdc2ceeb04645f65b736c302fc8b1
sysnet4admin <pagaia@hotmail.com>
2021-04-11 21:05:58 +0900

    add client crt

--- ch4/4.4.2/create-registry.sh
+++ ch4/4.4.2/create-registry.sh
@@ -12,7 +12,8 @@ for i in {1..3}
     sshpass -p vagrant ssh -o StrictHostKeyChecking=no root@192.168.1.10$i mkdir -p $certs
     sshpass -p vagrant scp tls.crt 192.168.1.10$i:$certs
   done
-
+  
+cp tls.crt $certs
 mv tls.* /etc/docker/certs
 
 docker run -d \
