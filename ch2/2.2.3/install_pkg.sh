a8530c6884aebde43a1de36b384585efd2b0d691
Hoon Jo <pagaia@hotmail.com>
2024-09-21 20:58:28 +0900

    upt CentOS repo change from mirror to vault

--- ch2/2.2.3/install_pkg.sh
+++ ch2/2.2.3/install_pkg.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
+
+# CentOS repo change from mirror to vault 
+sed -i -e 's/mirrorlist=/#mirrorlist=/g' /etc/yum.repos.d/CentOS-*
+sed -i -e 's/mirrorlist=/#mirrorlist=/g' /etc/yum.conf
+sed -E -i -e 's/#baseurl=http:\/\/mirror.centos.org\/centos\/\$releasever\/([[:alnum:]_-]*)\/\$basearch\//baseurl=https:\/\/vault.centos.org\/7.9.2009\/\1\/\$basearch\//g' /etc/yum.repos.d/CentOS-*
+sed -E -i -e 's/#baseurl=http:\/\/mirror.centos.org\/centos\/\$releasever\/([[:alnum:]_-]*)\/\$basearch\//baseurl=https:\/\/vault.centos.org\/7.9.2009\/\1\/\$basearch\//g' /etc/yum.conf
+
 # install packages 
 yum install epel-release -y
 yum install vim-enhanced -y
