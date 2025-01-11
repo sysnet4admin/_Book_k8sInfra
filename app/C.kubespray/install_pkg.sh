30f043a54a7fa022be4f145b9572696927505ad2
Hoon Jo <pagaia@hotmail.com>
2025-01-11 09:30:08 +0900

    chg mirrot to valut for repo as @snapsAhn 's comment

--- app/C.kubespray/install_pkg.sh
+++ app/C.kubespray/install_pkg.sh
@@ -1,6 +1,24 @@
 #!/usr/bin/env bash
 
-# install packages 
+# CentOS repo change from mirror to vault
+ sed -i -e 's/mirrorlist=/#mirrorlist=/g' /etc/yum.repos.d/CentOS-*
+ sed -i -e 's/mirrorlist=/#mirrorlist=/g' /etc/yum.conf
+ sed -E -i -e 's/#baseurl=http:\/\/mirror.centos.org\/centos\/\$releasever\/([[:alnum:]_-]*)\/\$basearch\//baseurl=https:\/\/vault.centos.org\/7.9.2009\/\1\/\$basearch\//g' /etc/yum.repos.d/CentOS-*
+ sed -E -i -e 's/#baseurl=http:\/\/mirror.centos.org\/centos\/\$releasever\/([[:alnum:]_-]*)\/\$basearch\//baseurl=https:\/\/vault.centos.org\/7.9.2009\/\1\/\$basearch\//g' /etc/yum.conf
+
+# kubernetes repo
+gg_pkg="http://mirrors.aliyun.com/kubernetes/yum" # Due to shorten addr for key
+cat <<EOF > /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=${gg_pkg}/repos/kubernetes-el7-x86_64
+enabled=1
+gpgcheck=0
+repo_gpgcheck=0
+gpgkey=${gg_pkg}/doc/yum-key.gpg ${gg_pkg}/doc/rpm-package-key.gpg
+EOF
+
+# install packages
 yum install epel-release -y
 yum install vim-enhanced -y
 yum install sshpass -y
