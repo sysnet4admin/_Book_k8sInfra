1b0924601a92ec3d27b0f3f2bad17c0d45be704b
Hoon Jo <pagaia@hotmail.com>
2026-03-19 09:40:43 +0900

    update IaC manifest paths in usage comments
    Follows IaC repo restructuring (sysnet4admin/IaC@ae5ec74)

--- app/A.kubectl-more/bash-completion.sh
+++ app/A.kubectl-more/bash-completion.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #Usage: 
-#1. bash <(curl -s https://raw.githubusercontent.com/sysnet4admin/IaC/master/manifests/bash-completion.sh) 
+#1. bash <(curl -s https://raw.githubusercontent.com/sysnet4admin/IaC/main/tools/bash-completion.sh) 
 
 # install bash-completion for kubectl 
 yum install bash-completion -y 
--- app/A.kubectl-more/k8s_rc.sh
+++ app/A.kubectl-more/k8s_rc.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # usage: 
 # 1. Create 
-# - bash <(curl -s  https://raw.githubusercontent.com/sysnet4admin/IaC/master/manifests/k8s_rc.sh) 
+# - bash <(curl -s  https://raw.githubusercontent.com/sysnet4admin/IaC/main/tools/k8s_rc.sh) 
 # 2. Remove 
 # - sed -i '/source/d' .bashrc
 
